80cce34b2abf7a6b00a99254103c0c3497d11590
shawnpinciara <shawnpinciara@gmail.com>
2023-04-07 13:15:54 +0200

    Mpr121 funzionante

--- ewi/arduino_code/EWI/EWI.ino
+++ ewi/arduino_code/EWI/EWI.ino
@@ -1,151 +1,62 @@
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_ESP32.h>
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+//LINUX port:
 //sudo chmod 0777 /dev/ttyACM0
+
+//PINS:
 //LEFT HAND: near:  15 - 2 - 4 - 32
 //RIGHT HAND: near: 12 - 14 - 27 - 33
-byte nota =  0b00000000;
-byte chiave = 0b00000000;
-byte mask1 = 0b00001000;
-byte mask2 = 0b00000100;
-byte mask3 = 0b00000010;
-byte mask4 = 0b00000001;
-// char note[] = {'C','D','B','E','C','A','F','G','C#','D#','U','U','E','A#','F#','G#'};
-int noteIndex[] = {0,1,11,4,12,9,5,7,2,3,0,0,16,10,6,8};
+
+
+//binary no midi note map es: binary:0001 -> decimal: 1 -->note: D (1 in midi value)
+const int noteArray[16] = {0,2,11,4,12,9,5,7,1,3,0,0,16,10,6,8};
 // int noteIndex[] = {1,3,12,5,1,10,6,8,2,4,0,0,5,11,7,9};
 char scale[] = {'C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B'};
-boolean ottavaSopra = false;
-int octave = 6;
+// boolean ottavaSopra = false;
+int octave = 5;
 int velocity = 127;
-const int numButtons = 8;  // The number of touch buttons you have
-const int buttonPins[numButtons] = { 15 ,2 , 4 , 32 , 12 , 14 , 27 , 33};  // The pins of the touch buttons
-const int midiNotes[numButtons] = { 60, 62, 64, 65 , 60+12, 62+12, 64+12, 65+12 };  // The MIDI notes to send when a button is pressed
-volatile bool buttonStates[numButtons] = { false ,false ,false ,false,false ,false ,false ,false };  // Initialize all buttons to be off
-volatile int currentNota = 0;
-int threshold = 25;
-
+int threshold = 30;
+uint8_t currentNote = 0;
+uint8_t lastNote = 120;
+uint8_t mpr121 = 0;
+Adafruit_MPR121 mpr = Adafruit_MPR121();
 BLEMIDI_CREATE_INSTANCE("EWI",MIDI);
 
-void pressSxButton1() {
-  nota = nota | mask1; 
-  MIDI.sendNoteOn(scale[noteIndex[int(nota)]+noteIndex[int(chiave)]-2]+octave, velocity, 1);  // Send a MIDI note on message
-}
-
-void releaseSxButton1() {
-  nota = nota & mask1; 
-  MIDI.sendNoteOn(scale[noteIndex[int(nota)]+noteIndex[int(chiave)]-2]+octave, velocity, 1);  // Send a MIDI note on message
-}
-
-void pressSxButton2() {
-  nota = nota | mask3; 
-  MIDI.sendNoteOn(scale[noteIndex[int(nota)]+noteIndex[int(chiave)]-2]+octave, velocity, 1);  // Send a MIDI note on message
-}
-
-void pressSxButton3() {
-  nota = nota | mask2; 
-  MIDI.sendNoteOn(scale[noteIndex[int(nota)]+noteIndex[int(chiave)]-2]+octave, velocity, 1);  // Send a MIDI note on message
-}
-
-void pressSxButton4() {
-  nota = nota | mask1; 
-  MIDI.sendNoteOn(scale[noteIndex[int(nota)]+noteIndex[int(chiave)]-2]+octave, velocity, 1);  // Send a MIDI note on message
-}
 
 void setup() {
   // put your setup code here, to run once:
   MIDI.begin(10);
-  Serial.begin(9600);
-  // attachInterrupt(digitalPinToInterrupt(touchRead(32)), pressSxButton1, RISING);
-  // attachInterrupt(digitalPinToInterrupt(touchRead(32)), releaseSxButton1, FALLING);
-  // attachInterrupt(digitalPinToInterrupt(touchRead(4)), pressSxButton2, RISING);
-  // attachInterrupt(digitalPinToInterrupt(touchRead(2)), pressSxButton3, RISING);
-  // attachInterrupt(digitalPinToInterrupt(touchRead(15)), pressSxButton4, RISING);
+  Serial.begin(115200);
 
+  //MPR121 setup
+  while (!Serial) { // needed to keep leonardo/micro from starting too fast!
+    delay(10);
+  }
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!mpr.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    int touchValue = touchRead(32);
-    if (touchValue < threshold) {  // If the button is pressed
-      if (!buttonStates[0]) {  // If the button state has changed to on
-        buttonStates[0] = true;  // Update the button state
-        // MIDI.sendNoteOn(midiNotes[buttonIndex], 127, 1);  // Send a MIDI note on message
-        nota = nota | mask4; 
-        MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-        MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-        currentNota = noteIndex[int(nota)]+octave*12;
-      }
-    }
-    else {  // If the button is released
-      if (buttonStates[0]) {  // If the button state has changed to off
-        buttonStates[0] = false;  // Update the button state
-        nota = nota - mask4; 
-        MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-        MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-        currentNota = noteIndex[int(nota)]+octave*12;
-        
-      }
-    }
-  touchValue = touchRead(4);
-  if (touchValue < threshold) {  // If the button is pressed
-      if (!buttonStates[1]) {  // If the button state has changed to on
-        buttonStates[1] = true;  // Update the button state
-        // MIDI.sendNoteOn(midiNotes[buttonIndex], 127, 1);  // Send a MIDI note on message
-        nota = nota | mask3; 
-        MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-        MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-        currentNota = noteIndex[int(nota)]+octave*12;
-      }
-    }
-    else {  // If the button is released
-      if (buttonStates[1]) {  // If the button state has changed to off
-        buttonStates[1] = false;  // Update the button state
-        nota = nota - mask3; 
-        MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-        MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-        currentNota = noteIndex[int(nota)]+octave*12;
-        
-      }
-    }
-  touchValue = touchRead(2);
-  if (touchValue < threshold) {  // If the button is pressed
-        if (!buttonStates[2]) {  // If the button state has changed to on
-          buttonStates[2] = true;  // Update the button state
-          // MIDI.sendNoteOn(midiNotes[buttonIndex], 127, 1);  // Send a MIDI note on message
-          nota = nota | mask2; 
-          MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-          MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-          currentNota = noteIndex[int(nota)]+octave*12;
-        }
-      }
-      else {  // If the button is released
-        if (buttonStates[2]) {  // If the button state has changed to off
-          buttonStates[2] = false;  // Update the button state
-          nota = nota - mask2; 
-          MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-          MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-          currentNota = noteIndex[int(nota)]+octave*12;
-        
-        }
-      }
-  touchValue = touchRead(14);
-  if (touchValue < threshold) {  // If the button is pressed
-        if (!buttonStates[3]) {  // If the button state has changed to on
-          buttonStates[3] = true;  // Update the button state
-          // MIDI.sendNoteOn(midiNotes[buttonIndex], 127, 1);  // Send a MIDI note on message
-          nota = nota | mask1; 
-          MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-          MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-          currentNota = noteIndex[int(nota)]+octave*12;
-        }
-      }
-      else {  // If the button is released
-        if (buttonStates[3]) {  // If the button state has changed to off
-          buttonStates[3] = false;  // Update the button state
-          nota = nota - mask1; 
-          MIDI.sendNoteOff(currentNota, 127, 1);  // Send a MIDI note off message
-          MIDI.sendNoteOn(noteIndex[int(nota)]+octave*12, velocity, 1);  // Send a MIDI note on message
-          currentNota = noteIndex[int(nota)]+octave*12;
-        
-        }
-      }
+
+  currentNote = mpr.touched(); //nota corrente cioè lavore letto dal sensore
+  if (currentNote != lastNote) { //se il valore letto da sensore è diverso da quello letto in precedenza
+    //fai smettere di suonare la nota precedente (perchè siamo in monofonia)
+    MIDI.sendNoteOff((octave*12)+noteArray[lastNote],velocity,1);
+    //aggiorna valore di nota precedente
+    lastNote = currentNote;
+    //inizia a suonare la nota premuta
+    MIDI.sendNoteOn((octave*12)+noteArray[currentNote], velocity, 1);  // Send a MIDI note 
+    Serial.println(scale[currentNote]); //log
+  }
+  //delay(500);
+    
 }
