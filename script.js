44e95a4bb2d73440f1dbabacc4f0900635dfdc6d
Shawn Pinciara <shawnpinciara@gmail.com>
2024-07-05 13:40:17 +0200

    Updates

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $("#dewi-ani").click(function(){
         window.open("./dewi/","_self")
-      });
+    })
+
+    $("#points-ani").click(function(){
+      window.open("https://pointscs.github.io/","_self")
+    })
+    
+    
 
 });
