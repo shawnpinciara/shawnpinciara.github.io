67a6a43bcbe227855374108ecb8ecb27bce2f1cf
unknown <shawnpinciara@gmail.com>
2024-10-08 10:11:07 +0200

    Updates

--- music/script.js
+++ music/script.js
@@ -2,7 +2,8 @@
 src_list = ["https://www.youtube.com/watch?v=gLs1ML3XOlE&list=PLJPbj8xFD1eM96jaxpnAVNqfHRYrqdB4B&pp=gAQBiAQB8AUB",
   "https://www.youtube.com/watch?v=8CwB4T1nkaM&list=PL0ORmUX32ARCg0wl_ho7hJK1sVEuxx6oF&pp=gAQBiAQB8AUB",
   "https://open.spotify.com/playlist/37i9dQZF1DWWjGdmeTyeJ6",
-  "https://open.spotify.com/playlist/37i9dQZEVXbqecL6jGt8wJ"
+  "https://open.spotify.com/playlist/37i9dQZEVXbqecL6jGt8wJ",
+  "https://www.radio.it/s/jazz24&data-autoplay=true"
 ];
 
 function getRndInteger(min, max) {
