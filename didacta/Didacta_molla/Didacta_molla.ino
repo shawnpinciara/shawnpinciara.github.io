4a62b9fe5203ffc7a322807846ff3b205204057d
Shawn <shawnpinciara@gmail.com>
2025-10-16 13:31:48 +0200

    didacta
    didacta

--- didacta/Didacta_molla/Didacta_molla.ino
+++ didacta/Didacta_molla/Didacta_molla.ino
@@ -11,16 +11,23 @@ ModulinoPixels leds;
 
 ModulinoDistance distance;
 
+// Create a ModulinoBuzzer object
+ModulinoBuzzer buzzer;
+
+
 void setup() {
     // 57600 is the default connection speed used in the JavaScript library.
   Serial.begin(57600);
   WebSerial.on("pixels", changeColor);
+  WebSerial.on("rec_on", recOn);
   // Initialize Modulino I2C communication
   Modulino.begin();
   // Detect and connect to pixels module
   leds.begin();
 
   distance.begin();
+
+  buzzer.begin();
   //Serial.begin(9600);
 }
 
@@ -34,6 +41,41 @@ void changeColor(JSONVar val) {
     }
 }
 
+void recOn(JSONVar val) {
+    if (int(val)>100) { //rec on
+        buzzer.tone(260, 500);
+    } else { //rec off
+        buzzer.tone(200, 500);
+    }
+}
+
+const int buffer_peak_size = 30; 
+
+int peak_buffer[buffer_peak_size];
+
+int buffer_index = 0;
+
+int sample_count = 0;
+
+bool findPeak(int sample) {
+    peak_buffer[buffer_index] = sample;
+    buffer_index = (buffer_index + 1) % buffer_peak_size;
+    if (sample_count < buffer_peak_size) {
+        sample_count++;
+        return false;
+    }
+    int center_index = (buffer_index - buffer_peak_size / 2 + buffer_peak_size) % buffer_peak_size;
+    int center_value = peak_buffer[center_index];
+    bool is_peak = true;
+    for (int i = 0; i < buffer_peak_size; i++) {
+        if (peak_buffer[i] > center_value) {
+            is_peak = false;
+            break; 
+        }
+    }
+    return is_peak;
+}
+
 
 int i =0;
 
@@ -45,6 +87,9 @@ void loop() {
     int measure = distance.get();
     //Serial.println(measure);
     WebSerial.send("event-from-arduino", measure);
+    if(findPeak(measure)) {
+      WebSerial.send("peak_found", measure);
+    }
   }
 
   // if (timer>300) {
