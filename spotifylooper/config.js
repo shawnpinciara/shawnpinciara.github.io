3799bed99d2c470ea254d564474e73e0fc5cfeea
shawnpinciara <shawnpinciara@gmail.com>
2026-07-27 23:53:50 +0200

    rename SpotifyLooper -> spotifylooper

--- spotifylooper/config.js
+++ spotifylooper/config.js
@@ -1,7 +1,7 @@
 const SPOTIFY_CLIENT_ID = '9e9bf7c4b17c4a48b48b947372103cd5';
 
-// Il redirect URI si adatta automaticamente all'ambiente:
-// - Sito:    https://shawnpinciara.github.io/SpotifyLooper
+// Redirect URI auto-adattato all'ambiente:
+// - Sito:    https://shawnpinciara.github.io/spotifylooper
 // - Locale:  http://127.0.0.1:3000
 // Aggiungi ENTRAMBI nel Spotify Dashboard -> Impostazioni -> Redirect URIs
 const REDIRECT_URI = window.location.origin + window.location.pathname.replace(/\/$/, '');
